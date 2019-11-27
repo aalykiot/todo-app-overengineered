f011554c399e98443a4a96a47715a9ffb9545961
alexalikiotis <alexalikiotis5@gmail.com>
2019-11-27 11:05:17 +0200

    Adding cors support

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import { importSchema } from 'graphql-import';
@@ -17,6 +18,8 @@ import './config/connection';
 import resolvers from './graphql/resolvers';
 
 const app = express();
+
+app.use(cors());
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
