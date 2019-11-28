36240bba9aea2d0e81b8957bf7dacf3e8ce061e6
alexalikiotis <alexalikiotis5@gmail.com>
2019-11-28 11:59:22 +0200

    Updating redux selectors

--- client/src/models/todos/selectors.js
+++ client/src/models/todos/selectors.js
@@ -1,8 +1,11 @@
-export const todosSelector = state => state.todos.items;
+export const todosSelector = ({ todos }) => todos.items;
 
-export const totalTodosSelector = state => state.todos.items.length;
+export const totalTodosSelector = ({ todos }) => todos.items.length;
 
-export const activeTodosSelector = state =>
-  state.todos.items.filter(todos => !todos.completed).length;
+export const activeTodosSelector = ({ todos }) =>
+  todos.items.filter(todos => !todos.completed).length;
 
-export const filterSelector = state => state.todos.filter;
+export const completedTodosSelector = ({ todos }) =>
+  todos.items.filter(todos => todos.completed).length;
+
+export const filterSelector = ({ todos }) => todos.filter;
