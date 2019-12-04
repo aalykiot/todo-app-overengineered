1053d8f0bd5f02e1d99391044f7652a2ca5a6467
alexalikiotis <alexalikiotis5@gmail.com>
2019-12-04 23:46:39 +0200

    Changing epics test file

--- client/src/__tests__/epics.test.js
+++ client/src/__tests__/epics.test.js
@@ -4,32 +4,26 @@ import * as actions from '../models/todos/actions';
 import * as epics from '../models/todos/epics';
 import * as apiService from '../services/api';
 
-// Log errors only to jest-virtual-console
 console.error = () => {};
 
-// Jest will mock the api service
 jest.mock('../services/api');
 
-// Preset test data
-const data = [
-  {
-    _id: 1,
-    text: 'Todo Number #1',
-    completed: false,
-  },
-  {
-    _id: 2,
-    text: 'Todo Number #2',
-    completed: true,
-  },
-];
+describe('epics', () => {
+  describe('load-todos', () => {
+    it('creates todos/LOAD_TODOS_SUCCESS when fetching todos has been done', done => {
+      const data = [
+        {
+          _id: 1,
+          text: 'Todo Number #1',
+          completed: false,
+        },
+        {
+          _id: 2,
+          text: 'Todo Number #2',
+          completed: true,
+        },
+      ];
 
-describe('Epics', () => {
-  // Testing redux observable epics
-  describe('Load todos epic', () => {
-    // Test the successful case of `loadTodosEpic`
-    it('should dispatch correct action when the todos have loaded', done => {
-      // Mock the api service's `getTodos` function
       apiService.getTodos.mockReturnValue(
         Promise.resolve({
           body: data,
@@ -47,86 +41,121 @@ describe('Epics', () => {
     });
   });
 
-  describe('Add todo epic', () => {
-    // Test the correct call of apiService `addTodo` function
-    it('should correctly call `apiService` to add todo', done => {
-      // Mock the apiService `addTodo` response
+  describe('add-todo', () => {
+    it('should call apiService correctly', done => {
       apiService.addTodo.mockReturnValue(Promise.resolve({}));
 
-      const action$ = of(actions.addTodoRequest(data[0].text));
+      const text = 'Todo Number #1';
+
+      const action$ = of(actions.addTodoRequest(text));
       const epic$ = epics.addTodoEpic(action$);
 
       epic$.subscribe(() => {
-        expect(apiService.addTodo.mock.calls[0][0]).toEqual(data[0].text);
+        expect(apiService.addTodo.mock.calls[0][0]).toEqual(text);
         done();
       });
     });
 
     // Test the successful case of `addTodoEpic`
-    it('should dispatch correct action when a todo has been added', done => {
-      // Mock the api service's `addTodo` function
+    it('creates todos/ADD_TODO_SUCCESS when a new todo has been added', done => {
+      const data = {
+        _id: 1,
+        text: 'Todo Number #1',
+        completed: false,
+      };
+
       apiService.addTodo.mockReturnValue(
         Promise.resolve({
-          body: data[0],
+          body: data,
         })
       );
 
-      const action$ = of(actions.addTodoRequest());
+      const action$ = of(actions.addTodoRequest(data.text));
       const epic$ = epics.addTodoEpic(action$);
 
       epic$.subscribe(action => {
         expect(action.type).toEqual(actions.addTodoSuccess.type);
-        expect(action.payload).toEqual(data[0]);
+        expect(action.payload).toEqual(data);
         done();
       });
     });
   });
 
-  describe('Toggle todo epic', () => {
-    // Test the correct call of apiService `toggleTodo` function
-    it('should correctly call `apiService` to toggle todo', done => {
-      // Mock the apiService `toggleTodo` response
-      apiService.toggleTodo.mockReturnValue(Promise.resolve({}));
+  describe('toggle-todo', () => {
+    it('should call apiService correctly', done => {
+      const todo = {
+        _id: 1,
+        text: 'Todo Number #1',
+        completed: false,
+      };
 
-      const action$ = of(actions.toggleTodoRequest(data[0]));
+      const response = {
+        _id: 1,
+        text: 'Todo Number #1',
+        completed: true,
+      };
+
+      apiService.toggleTodo.mockReturnValue(
+        Promise.resolve({ body: response })
+      );
+
+      const action$ = of(actions.toggleTodoRequest(todo));
       const epic$ = epics.toggleTodoEpic(action$);
 
       epic$.subscribe(() => {
-        expect(apiService.toggleTodo.mock.calls[0][0]).toEqual(data[0]);
+        expect(apiService.toggleTodo.mock.calls[0][0]).toEqual(todo);
         done();
       });
     });
 
-    // Test the successful case of `toggleTodoEpic`
-    it('should dispatch correct action when a todo has been toggled', done => {
-      // Mock the apiService `toggleTodo` response
+    it('creates todos/TOGGLE_TODO_SUCCESS when a todo has been toggled', done => {
+      const todo = {
+        _id: 1,
+        text: 'Todo Number #1',
+        completed: false,
+      };
+
+      const response = {
+        _id: 1,
+        text: 'Todo Number #1',
+        completed: true,
+      };
+
       apiService.toggleTodo.mockReturnValue(
         Promise.resolve({
-          body: { ...data[0], completed: !data[0].completed },
+          body: response,
         })
       );
 
-      const action$ = of(actions.toggleTodoRequest(data[0]));
+      const action$ = of(actions.toggleTodoRequest(todo));
       const epic$ = epics.toggleTodoEpic(action$);
 
       epic$.subscribe(action => {
         expect(action.type).toEqual(actions.toggleTodoSuccess.type);
-        expect(action.payload).toEqual({
-          ...data[0],
-          completed: !data[0].completed,
-        });
+        expect(action.payload).toEqual(response);
         done();
       });
     });
   });
 
-  describe('Toggle all todos epic', () => {
-    // Test the successful case of `toggleAllEpic`
-    it('should dispatch correct action when all todo has been toggled', done => {
-      // Mock the apiService `toggleAllTodos` response
+  describe('toggle-all-todos', () => {
+    it('creates todos/TOGGLE_ALL_SUCCESS when all todos have been toggled', done => {
+      const data = [
+        {
+          _id: 1,
+          text: 'Todo Number #1',
+          completed: false,
+        },
+        {
+          _id: 2,
+          text: 'Todo Number #2',
+          completed: true,
+        },
+      ];
+
       apiService.toggleAllTodos.mockReturnValue(
         Promise.resolve({
-          body: data.map(item => ({ ...item, completed: true })),
+          body: data,
         })
       );
 
@@ -135,56 +164,74 @@ describe('Epics', () => {
 
       epic$.subscribe(action => {
         expect(action.type).toEqual(actions.toggleAllSuccess.type);
-        expect(action.payload).toEqual(
-          data.map(item => ({ ...item, completed: true }))
-        );
+        expect(action.payload).toEqual(data);
         done();
       });
     });
   });
 
-  describe('Remove todo epic', () => {
-    // Test the correct call of apiService `removeTodo` function
-    it('should correctly call `apiService` to remove todo', done => {
-      // Mock the apiService `removeTodo` response
-      apiService.removeTodo.mockReturnValue(Promise.resolve({}));
+  describe('remove-todo', () => {
+    it('should call apiService correctly', done => {
+      const todo = {
+        _id: 1,
+        text: 'Todo Number #1',
+        completed: false,
+      };
 
-      const action$ = of(actions.removeTodoRequest(data[0]));
+      // Mock the apiService `removeTodo` response
+      apiService.removeTodo.mockReturnValue(Promise.resolve({ body: todo }));
+
+      const action$ = of(actions.removeTodoRequest(todo));
       const epic$ = epics.removeTodoEpic(action$);
 
       epic$.subscribe(() => {
-        expect(apiService.removeTodo.mock.calls[0][0]).toEqual(data[0]);
+        expect(apiService.removeTodo.mock.calls[0][0]).toEqual(todo);
         done();
       });
     });
 
-    // Test the successful case of `removeEpic`
-    it('should dispatch correct action when a todo has been removed', done => {
-      // Mock the apiService `removeTodos` response
+    it('creates todos/REMOVE_TODO_SUCCESS when a todo has been removed', done => {
+      const todo = {
+        _id: 1,
+        text: 'Todo Number #1',
+        completed: true,
+      };
+
       apiService.removeTodo.mockReturnValue(
         Promise.resolve({
-          body: data[0],
+          body: todo,
         })
       );
 
-      const action$ = of(actions.removeTodoRequest(data[0]));
+      const action$ = of(actions.removeTodoRequest(todo));
       const epic$ = epics.removeTodoEpic(action$);
 
       epic$.subscribe(action => {
         expect(action.type).toEqual(actions.removeTodoSuccess.type);
-        expect(action.payload).toEqual(data[0]);
+        expect(action.payload).toEqual(todo);
         done();
       });
     });
   });
 
-  describe('Remove completed todos epic', () => {
-    // Test the successful case of `removeCompletedEpic`
-    it('should dispatch correct action when completed todos have been removed', done => {
-      // Mock the apiService `removeCompletedTodos` response
+  describe('remove-completed-todos', () => {
+    it('creates todos/REMOVE_COMPLETED_SUCCESS when all completed todos have been removed', done => {
+      const data = [
+        {
+          _id: 1,
+          text: 'Todo Number #1',
+          completed: true,
+        },
+        {
+          _id: 2,
+          text: 'Todo Number #2',
+          completed: true,
+        },
+      ];
+
       apiService.removeCompletedTodos.mockReturnValue(
         Promise.resolve({
-          body: [data[1]],
+          body: data,
         })
       );
 
@@ -193,7 +240,7 @@ describe('Epics', () => {
 
       epic$.subscribe(action => {
         expect(action.type).toEqual(actions.removeCompletedSuccess.type);
-        expect(action.payload).toEqual([data[1]]);
+        expect(action.payload).toEqual(data);
         done();
       });
     });
